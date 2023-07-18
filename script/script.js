b0230e09935295402d9f09a37ad5812b9a48f5ae
Arun Kumar R <aruntutter19@gmail.com>
2023-07-18 17:26:35 +0530

    random color function updated

--- script/script.js
+++ script/script.js
@@ -1,25 +1,23 @@
 "use strict";
 
 // get elements
-const wrap = document.querySelector('body')
+const wrap = document.querySelector("body");
 const randomHex = document.getElementById("random-hex");
 const btn = document.getElementById("btn");
 
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-btn.addEventListener("click", changeBG);
-
-// Random Color
-function changeBG() {
+// Random Hex color
+btn.addEventListener("click", function () {
   let hexColor = "#";
   for (let i = 1; i <= 6; i++) {
     hexColor = hexColor + randomHexValue();
   }
   wrap.style.backgroundColor = hexColor;
   randomHex.innerHTML = hexColor;
-}
+});
 
-// Random Hex
+// Random Hex Number
 function randomHexValue() {
   let randomIndex = Math.floor(Math.random() * 16);
   return hex[randomIndex];
