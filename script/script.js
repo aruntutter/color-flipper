89f86eb6689f6d106dcef1274514c02f026459ce
Arun Kumar R <aruntutter19@gmail.com>
2023-07-18 17:15:28 +0530

    js logic completed

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+// get elements
+const wrap = document.querySelector('body')
+const randomHex = document.getElementById("random-hex");
+const btn = document.getElementById("btn");
+
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+btn.addEventListener("click", changeBG);
+
+// Random Color
+function changeBG() {
+  let hexColor = "#";
+  for (let i = 1; i <= 6; i++) {
+    hexColor = hexColor + randomHexValue();
+  }
+  wrap.style.backgroundColor = hexColor;
+  randomHex.innerHTML = hexColor;
+}
+
+// Random Hex
+function randomHexValue() {
+  let randomIndex = Math.floor(Math.random() * 16);
+  return hex[randomIndex];
+}
